2261db6b38791d2c9dc97b024a9ed2322ca0f165
Brandon <files.brandon@gmail.com>
2021-05-22 12:21:28 -0500

    fleshed out project slightly more, need to fix objects

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -1,5 +1,73 @@
 package com.skilldistillery.app;
 
 public class FoodTruck {
+	/*
+	 * You will define a FoodTruck class with fields for a unique numeric id, a name
+	 * ("TacoRific", "Mediterranean Medic", etc.), food type ("Tacos", "Falafel",
+	 * etc.), and a numeric rating.
+	 */
+
+	private static int numID = 0; // unique numeric id 
+	public String name; // "TacoRific", etc
+	public String foodType; // "Tacos"
+	public int rating; // 1 - 5 stars
+	public String[] allTrucks;
+	
+	public FoodTruck() {}; //no argument constructor, may be unnecessary to run program
+		{
+			this.numID = newNumID();
+		}
+		
+	public FoodTruck(int numID, String name, String foodType, int rating) {
+			this.numID = newNumID(); //this may take care of having to increment each ID individually
+			this.name = name; 
+			this.foodType = foodType; 
+			this.rating = rating;
+	}
+
+
+	public String[] listAllFoodTrucks(){
+		return allTrucks;
+	
+	}
+
+	public double averageRatingFoodTrucks() {
+		    return 0.0;
+		  }
+
+//	public void highestRatedFoodTruck() {
+//		balance = balance + amount;
+//	}
+
+
+	
+	public int getNumID() {
+		return numID;
+	}
+	public int newNumID() {
+		this.numID = numID++;
+		return numID; 
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFoodType() {
+		return foodType;
+	}
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	
 
 }
--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -1,10 +1,101 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		/*
+		 * User Story #1 
+		 * The user is prompted to input the name, food type, and rating
+		 * for up to five food trucks. For each set of input, a FoodTruck object is
+		 * created, its fields set to the user's input, and it is added to the array.
+		 * The truck id is not input by the user, but instead assigned automatically in
+		 * the FoodTruck constructor from a static field that is incremented as each
+		 * truck is created. 
+		 * */
+		
+		/*
+		 * User Story #2 
+		 * If the user inputs quit for the food truck
+		 * name, input ends immediately and the program continues. 
+		 * 
+		 * */
+		
+		String input = ""; //TODO change this to a stringBuilder
+		for (int i = 1; i <= 5; i++) {
+				System.out.println("*** Please input the name, food type, and your rating for food truck " + i +" ***");
+				System.out.println("Name " + i + ": ");
+				//TODO turn this into a method for input validation
+				input = scanner.next();
+				if(input.equalsIgnoreCase("Quit") || input.equalsIgnoreCase("Q")) { //checks to see if user input quit
+					break; //moves to next part of program
+				}else {
+					FoodTruck truck = new FoodTruck(); //initialize an instance of truck 
+					truck.setName(input); //inputs name from earlier
+					System.out.println("What kind of food does " + truck.getName() +" serve?: ");
+					String foodType = scanner.next(); //next line causing issues, next instead
+					truck.setFoodType(foodType);
+					System.out.print("Your rating (1 - 5): ");
+					int rating = scanner.nextInt();
+					truck.setRating(rating);
+					System.out.println();
+					
+				}
+				
+			}
+		
+		
+		/*
+		 * User Story #3 
+		 * After input is complete, the user sees a menu from which they can choose to:
+		 * List all existing food trucks. See the average rating of food trucks. Display
+		 * the highest-rated food truck. Quit the program. 
+		
+		
+		 * User Story #4 
+		 * After choosing a menu item, the user sees the menu again and can choose another item until
+		 * they choose to quit.
+		 */
 
+		//TODO displayMenu method
+		int userChoice = 0;
+		FoodTruck choice = new FoodTruck();
+		while(userChoice != 4) {
+			System.out.println();
+			System.out.println("************************************");
+			System.out.println("|1.) List all existing food trucks |");
+			System.out.println("|2.) Average rating of food trucks |");
+			System.out.println("|3.) Highest rated food trucks 	   |");
+			System.out.println("|4.) Quit                          |");
+			System.out.println("************************************");
+			System.out.print("\nPlease enter a selection: ");
+			userChoice = scanner.nextInt();
+			switch(userChoice){
+			case 1:
+				choice.listAllFoodTrucks();
+				break;
+			case 2:
+				choice.averageRatingFoodTrucks();
+				break;
+			case 3:
+				break;
+			case 4:
+				System.out.println("Thank you, Come Again!");
+				break;
+			default:
+				System.out.println("Error: Invalid input.");
+				break;
+			}
+		}
+
+
+	
 	}
+	
+	
+	
 
 }
