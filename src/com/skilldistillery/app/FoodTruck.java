e7c4a26cb007324cbde62eceaeb911d009851d7b
Brandon <files.brandon@gmail.com>
2021-05-23 18:10:07 -0500

    Finished up the homework assignment. Need to refactor code blocks

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -1,18 +1,9 @@
 package com.skilldistillery.app;
 
-import java.util.Arrays;
-
 public class FoodTruck {
-	/*
-	 * You will define a FoodTruck class with fields for a unique numeric id, a name
-	 * ("TacoRific", "Mediterranean Medic", etc.), food type ("Tacos", "Falafel",
-	 * etc.), and a numeric rating.
-	 */
 	
 	//FIELDS
-	private FoodTruck[] foodTrucksReviewed; //array of trucks
-	private static int MAX_FOODTRUCKS = 5;	
-	private static int count = 0; //increments each time constructor is called
+	private static int count = 0; //increments each time the constructor is called
 	private int numID = 0; // unique numeric id based off count
 	public String name; 
 	public String foodType; 
@@ -22,66 +13,10 @@ public class FoodTruck {
 	//CONSTRUCTORS
 	public FoodTruck() {
 		numID = count++;
-	}; // no argument constructor, may be unnecessary to run program
-
+	}; 
 	
-	public FoodTruck(int numID, String name, String foodType, int rating) {
-		this.numID = numID++; // this may take care of having to increment each ID individually
-		this.name = name;
-		this.foodType = foodType;
-		this.rating = rating;
-	}
-
-	
-	//METHODS
-	public int addFoodTruck(FoodTruck t) {
-		foodTrucksReviewed[numID] = t; 
-		numID++;
-		return numID;
-	}
-	
-	public FoodTruck[] listAllFoodTrucks() {
-		FoodTruck[] trucksCopy = new FoodTruck[numID];
-		//initializes an array the size of the food trucks input
-	    for(int i=0; i < numID; i++) {
-	      trucksCopy[i] = foodTrucksReviewed[i];
-	    }
-	    
-	    return trucksCopy; //returns a copy of food trucks
-		//only return values that aren't null!
 		
-
-	}
-	
-	
-	public double averageRatingFoodTrucks() {
-		System.out.println(0.0);
-		return 0.0;
-	}
-
-//	public void highestRatedFoodTruck() {
-//		balance = balance + amount;
-//	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("FoodTruck [name=");
-		builder.append(foodTrucksReviewed[0].name);
-		builder.append(", foodType=");
-		builder.append(foodTrucksReviewed[0].foodType);
-		builder.append(", rating=");
-		builder.append(foodTrucksReviewed[0].rating);
-		builder.append(", allTrucks=");
-		builder.append(Arrays.toString(foodTrucksReviewed));
-		builder.append("]");
-		return builder.toString();
-	}
-
-	public void newTruckID(FoodTruck t) {
-		foodTrucksReviewed[numID] = t;
-		numID++; // increment to reflect that we have one more truck
-	}
+	//METHODS
 
 	public int getNumID() {
 		return numID;
@@ -118,3 +53,51 @@ public class FoodTruck {
 	}
 
 }
+//	public int addFoodTruck(FoodTruck t) {
+//		foodTrucksReviewed[numID] = t; 
+//		numID++;
+//		return numID;
+//	}
+
+//	public FoodTruck[] listAllFoodTrucks() {
+//		FoodTruck[] trucksCopy = new FoodTruck[numID];
+//		//initializes an array the size of the food trucks input
+//	    for(int i=0; i < numID; i++) {
+//	      trucksCopy[i] = foodTrucksReviewed[i];
+//	    }
+//	    
+//	    return trucksCopy; //returns a copy of food trucks
+//		//only return values that aren't null!
+//		
+//
+//	}
+
+
+//	public double averageRatingFoodTrucks() {
+//		System.out.println(0.0);
+//		return 0.0;
+//	}
+
+//	public void highestRatedFoodTruck() {
+//		balance = balance + amount;
+//	}
+
+//	@Override
+//	public String toString() {
+//		StringBuilder builder = new StringBuilder();
+//		builder.append("FoodTruck [name=");
+//		builder.append(foodTrucksReviewed[0].name);
+//		builder.append(", foodType=");
+//		builder.append(foodTrucksReviewed[0].foodType);
+//		builder.append(", rating=");
+//		builder.append(foodTrucksReviewed[0].rating);
+//		builder.append(", allTrucks=");
+//		builder.append(Arrays.toString(foodTrucksReviewed));
+//		builder.append("]");
+//		return builder.toString();
+//	}
+
+//	public void newTruckID(FoodTruck t) {
+//		foodTrucksReviewed[numID] = t;
+//		numID++; // increment to reflect that we have one more truck
+//	}
--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -4,68 +4,49 @@ import java.util.Scanner;
 
 public class FoodTruckApp {
 
+	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		/*
-		 * User Story #1 
-		 * The user is prompted to input the name, food type, and rating
-		 * for up to five food trucks. For each set of input, a FoodTruck object is
-		 * created, its fields set to the user's input, and it is added to the array.
-		 * 
-		 * The truck id is not input by the user, but instead assigned automatically in
-		 * the FoodTruck constructor from a static field that is incremented as each
-		 * truck is created. 
-		 * 
-		 * */
-		
-		
-		/*
-		 * User Story #2 
-		 * If the user inputs quit for the food truck
-		 * name, input ends immediately and the program continues. 
-		 * 
-		 */
 		
 		String input = ""; //TODO change this to a stringBuilder to save memory
-		int numOfreviews = 5; //may need to change based on how many inputs user does
-		FoodTruck[] trucks = new FoodTruck[numOfreviews]; //array of 5 FoodTrucks objects created called trucks
 		
-		for (int i = 1; i <= numOfreviews; i++) {
-				System.out.println("*** Please input the name, food type, and your rating for food truck " + i +" ***");
-				System.out.println("Name " + i + ": ");
-				input = scanner.next(); //TODO turn this into a method for input validation
+		int numOfReviews = 5; //may be able to move to a separate class
+		FoodTruck[] trucks = new FoodTruck[numOfReviews]; //array of 5 FoodTrucks objects created called trucks
+		
+		//reviewTrucks(numeOfReviews); 
+		for (int i = 0; i < numOfReviews; i++) {
+				System.out.println("*** Food truck " + (i+1) + " - Please input the name, food type, and your rating: ***");
+				System.out.println("Name: ");
+				input = scanner.nextLine(); //TODO turn this into a method for input validation
 				if(input.equalsIgnoreCase("Quit") || input.equalsIgnoreCase("Q")) { //checks to see if user input 'quit'
 					break; //moves to menu part of program
 				}else {
 					trucks[i] = new FoodTruck(); //initialize an instance of FoodTruck in the array 
 					trucks[i].setName(input); //inputs name from earlier
 					System.out.println("What kind of food does " + trucks[i].getName() +" serve?: ");
-					String foodType = scanner.next(); //next line causing issues, next instead
+					String foodType = scanner.nextLine(); //next line causing issues, next instead
 					trucks[i].setFoodType(foodType);
 					System.out.print("Your rating (1 - 5): ");
-					int rating = scanner.nextInt();
-					trucks[i].setRating(rating);
-					System.out.println();
-					
+					while(true) {
+						int rating = Integer.parseInt(scanner.nextLine());
+						if(rating > 0 && rating < 6) {
+							trucks[i].setRating(rating);
+							break;
+						} else {
+							System.out.println("Invalid rating, please enter a valid number between 1 - 5");
+						}
+					}
 				}
 				
 			}
 		
 		
-		/*
-		 * User Story #3 
-		 * After input is complete, the user sees a menu from which they can choose to:
-		 * List all existing food trucks. See the average rating of food trucks. Display
-		 * the highest-rated food truck. Quit the program. 
-		
-		
-		 * User Story #4 
-		 * After choosing a menu item, the user sees the menu again and can choose another item until
-		 * they choose to quit.
-		 */
 
-		//TODO displayMenu method
+
+		
+	//TODO displayMenu method
+	
+	//public void displayMenu() {
 		int userChoice = 0;
 		while(userChoice != 4) {
 			System.out.println();
@@ -79,33 +60,45 @@ public class FoodTruckApp {
 			userChoice = scanner.nextInt();
 			switch(userChoice){
 			case 1:
-				System.out.println(trucks.length);
+				
 				for (int i = 0; i < trucks.length; i++) {
 					if (trucks[i] != null) {
-					System.out.println(trucks[i].getName());
+						System.out.println(trucks[i].getName());
 					}
 				}
 				break;
+				
 			case 2:
 				//trucks.averageRatingFoodTrucks();
-				int counter = 0;
+				int sumOfRatings = 0;
+				double divCounter = 0;
 				for (int i = 0; i < trucks.length; i++) {
 					if (trucks[i] != null) {
-						
-						System.out.println(trucks[i].getRating());
+						sumOfRatings += trucks[i].getRating();
+						divCounter++;
 					}
 				}
+				double averageRating =  (double)(sumOfRatings / divCounter);
+				System.out.println(averageRating);
+				
 				break;
+				
 			case 3:
 				//trucks.highestRated(); 
-				FoodTruck highestRated = trucks[0];
+				int highestRating = trucks[0].rating;
+				String highestRatedName = trucks[0].name;
 				for (int i = 0; i < trucks.length; i++) {
-					if (trucks[i].rating > highestRated.rating) {
-						highestRated = trucks[i];
+					if (trucks[i] != null) {
+						if (trucks[i].rating > highestRating) {
+							highestRating = trucks[i].rating;
+							highestRatedName = trucks[i].name;
+							
+						}
 					}
-					System.out.println(highestRated.getRating());
 				}
+				System.out.println("The highest rated truck is " + highestRatedName + " with a score of " + highestRating + "!");
 				break;
+				
 			case 4:
 				System.out.println("Thank you, Come Again!");
 				break;
@@ -115,10 +108,6 @@ public class FoodTruckApp {
 			}
 		}
 
-	
 	}
-	
-	
-	
-
+		
 }
