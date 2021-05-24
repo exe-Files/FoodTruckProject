2f8e5534bb5b40f9af93749fc0a74c5348c91c02
Brandon <files.brandon@gmail.com>
2021-05-24 01:02:43 -0500

    Fixed an issue with Eclipse not recognizing the public constructor as legitimate

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -1,28 +1,33 @@
 package com.skilldistillery.app;
 
 public class FoodTruck {
-	
-	//FIELDS
-	private static int count = 0; //increments each time the constructor is called
-	private int numID = 0; // unique numeric id based off count
-	public String name; 
-	public String foodType; 
-	public int rating;h
 
-	
-	//CONSTRUCTORS
+	// FIELDS
+	private static int count = 0; // increments each time the constructor is called
+	private int numID = 0; // unique numeric id based off count
+	public String name;
+	public String foodType;
+	public int rating;
+
+	// CONSTRUCTOR
 	public FoodTruck() {
-		numID = count++;
-	}; 
-	
-		
-	//METHODS
-	
+		numID = count++; //increments and then is assigned to numID
+	};
+
+	public FoodTruck(int numID, String name, String foodType, int rating) {
+		this.numID = count++;
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+	}
+
+	// METHODS
+
 	@Override
 	public String toString() {
 		return "FoodTruck [numID=" + numID + ", name=" + name + ", foodType=" + foodType + ", rating=" + rating + "]";
 	}
-	
+
 //	@Override
 //	public String toString() {
 //		StringBuilder builder = new StringBuilder();
@@ -37,12 +42,10 @@ public class FoodTruck {
 //		builder.append("]");
 //		return builder.toString();
 //	}
-	
+
 	public int getNumID() {
 		return numID;
 	}
-
-
 
 	public int newNumID(int numID) {
 		this.numID = numID;
@@ -75,5 +78,3 @@ public class FoodTruck {
 	}
 
 }
-
-
